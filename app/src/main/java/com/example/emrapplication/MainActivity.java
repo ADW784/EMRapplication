88b179d3742a651dcdd0c628a6be7334fb8f4bf3
Akeem Williams <akeem.d.williams@outlook.com>
2019-12-01 18:53:03 -0400

    login Link Push 2

--- app/src/main/java/com/example/emrapplication/MainActivity.java
+++ app/src/main/java/com/example/emrapplication/MainActivity.java
@@ -6,15 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonRegister;
+    TextView textViewLoginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewLoginLink = findViewById(R.id.textViewLoginLink);
+        textViewLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //default code. simply takes you to the next activity.
+                Intent i = new Intent(MainActivity.this,Login.class);
+                startActivity(i);
+            }
+        });
 
         buttonRegister = findViewById(R.id.buttonRegister);
         buttonRegister.setOnClickListener(new View.OnClickListener() {
